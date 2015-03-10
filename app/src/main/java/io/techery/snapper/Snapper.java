1340fd9a6162cce45b36d7e978d72e43f8e1f64d
Aleksey Malevaniy <a.malevaniy@gmail.com>
2015-03-10 19:44:00 +0200

    Double check DataCollection on Snapper#collection() cache access
    Without sync block there is situation when many threads are accessing collection() method and cache is not ready, so
    each thread potentially could create it's own datacollection and only last will be placed to cache then.

--- app/src/main/java/io/techery/snapper/Snapper.java
+++ app/src/main/java/io/techery/snapper/Snapper.java
@@ -10,28 +10,30 @@ import io.techery.snapper.storage.StorageFactory;
 public abstract class Snapper {
 
     private final StorageFactory storageFactory;
-    private final Map<Class, DataCollection> dataCollectionCache = new HashMap<>();
+    private final Map<Class<? extends Indexable>, DataCollection> dataCollectionCache;
 
     public Snapper(StorageFactory storageFactory) {
         this.storageFactory = storageFactory;
+        dataCollectionCache = new HashMap<>();
     }
 
     public <T extends Indexable> DataCollection<T> collection(Class<T> className) {
-
         DataCollection<T> dataCollection = dataCollectionCache.get(className);
-
         if (dataCollection == null) {
-
-            try {
-                dataCollection = new DataCollection<>(storageFactory.createStorage(className));
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
+            // Double check not to create duplicates from sep. threads
+            synchronized (Snapper.class) {
+                dataCollection = dataCollectionCache.get(className);
+                if (dataCollection == null) {
+                    try {
+                        dataCollection = new DataCollection<>(storageFactory.createStorage(className));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                    dataCollectionCache.put(className, dataCollection);
+                }
             }
-
-            dataCollectionCache.put(className, dataCollection);
         }
-
         return dataCollection;
     }
 }
